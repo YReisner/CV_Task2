organized the code: deleted all useless code. Moved parameters to params. wrote the precision-recall function, did not run the code.. tried to fix the errors function in a way that saves the indices..unsuccessfully will try again later

taskTransfer.py
@@ -19,16 +19,24 @@ def GetDefaultParameters():
     labels = scipy.io.loadmat(path + '\FlowerDataLabels.mat')["Labels"]
     image_size = (224,224)
     split = 0.2
-
-    clusters = 40
-    svm_c = 100
-    degree = 3
-    kernel = 'rbf'
-    gamma = 5
-    step_size = 6
-    bins = clusters
+    input_shape = (224,224,3)
+    activation ='sigmoid'
+    test_size = 0.2
     validate = False
-    parameters = {"path":path,"Labels":labels,"test_indices":test_indices,"validate":validate,"image_size":image_size,"Split":split,"clusters":clusters,"step_size":step_size,"bins":bins, "svm_c":svm_c,"kernel":kernel,"gamma":gamma,'degree':degree}
+    momentum = 0.5
+    learning_rate = 0.001
+    weights =  'imagenet'
+    metrics = 'accuracy'
+    batch_size = 35
+    loss =  'binary_crossentropy'
+    epochs = 10
+    random_state = 42
+    test_batch_size = 32
+    parameters = {"path":path,"Labels":labels,"test_indices":test_indices,"validate":validate,"image_size":image_size,
+                  "Split":split,"input_shape":input_shape, "activation":activation,"test_size":test_size,"momentum":momentum,
+                  "learning_rate":learning_rate,"weights":weights,"loss":loss,"metrics":metrics,"batch_size":batch_size,"epochs":epochs,
+                  "random_state":random_state,"test_batch_size":test_batch_size}
+
     return parameters
 
 
@@ -56,12 +64,8 @@ def load_data(params):
             test_labels.append(params['Labels'][0][i-1])
     train_images = np.asarray(train_images)
     train_labels = np.asarray(train_labels)
-    #plt.imshow(train_images[54][:,:,[2,1,0]])
-    #plt.show()
     test_images = np.asarray(test_images)
     test_labels = np.asarray(test_labels)
-    print(test_images.shape)
-    print(test_labels.shape)
     print("Data loading complete!")
     return train_images,train_labels,test_images,test_labels
 
@@ -76,52 +80,28 @@ def train_model(params,data,labels):
     :return: Returns a trained Resnet50v2 model, with a new top layer
     '''
 
-    base_model = keras.applications.resnet_v2.ResNet50V2(include_top = False, weights = 'imagenet', input_shape = (224,224,3)) #load the base model
+    base_model = keras.applications.resnet_v2.ResNet50V2(include_top = False, weights = params["weights"], input_shape =params["input_shape"] ) #load the base model
     base_model.trainable = False #Freezing the existing weights
 
     array_data = np.array(data)
     array_labels = np.array(labels)
-    print(data.shape)
-    print(labels.shape)
-    train_x, val_x, train_y, val_y = train_test_split(array_data,array_labels,test_size=0.2,random_state=42)
 
+    train_x, val_x, train_y, val_y = train_test_split(array_data,array_labels,params["test_size"],random_state=params["random_state"])
     train_features = base_model(train_x[0:1]) # create final ResNet features to jump-start the sequential layers
-
     global_max_layer = keras.layers.GlobalMaxPooling2D() #Adding a convolutional layer to create somthing a global layer can handle
     feature_batch_average = global_max_layer(train_features) # convolutional layer that aggregates high dimensional data to a vector
-    print(feature_batch_average.shape)
-    prediction_layer = keras.layers.Dense(1, activation='sigmoid') # take vector and create predictions
+    prediction_layer = keras.layers.Dense(1, params["activation"]) # take vector and create predictions
     prediction_batch = prediction_layer(feature_batch_average)
-    print(prediction_batch.shape)
+    model = keras.Sequential([base_model,global_max_layer,prediction_layer]) # add sequntial layers to ResNet to create the sequential model
 
-    model = keras.Sequential([
-        base_model,
-        global_max_layer,
-        prediction_layer
-    ]) # add sequntial layers to ResNet to create the sequential model
-
-
-    base_learning_rate = 0.001 # hyper parameter
-    model.compile(optimizer=keras.optimizers.SGD(lr=base_learning_rate,momentum=0.5),
-                  loss='binary_crossentropy',
-                  metrics=['accuracy']) # define the learning parameters
+   # hyper parameter
+    model.compile(optimizer=keras.optimizers.SGD(lr= params["base_learning_rate"],momentum = params["momentum"]),
+                  loss = params["loss"],metrics=params["metrics"]) # define the learning parameters
 
     print(model.summary()) # how does our sequential model looks like?
+    # train the model, and retain the results at every step
+    history = model.fit(train_x, train_y,batch_size=params["batch_size"], epochs=params["epochs"], shuffle=True, validation_data=(val_x,val_y))
 
-    initial_epochs = 10 #number of time running over all the data in training
-    validation_steps = 2 # this is for the evaluation. If we had more data, we could raise this for better results
-    '''
-    loss0, accuracy0 = model.evaluate(validation,validation_labels,batch_size=20,steps=validation_steps) # give the accuracy before training
-
-    print("initial loss: {:.2f}".format(loss0))
-    print("initial accuracy: {:.2f}".format(accuracy0))
-    '''
-    history = model.fit(train_x, train_y,batch_size=35,
-                        epochs=initial_epochs,
-                        shuffle=True,
-                        validation_data=(val_x,val_y)) #train the model, and retain the results at every step
-    print("this is the input shape")
-    print(model.input_shape)
     # Just for printing the results, will be moved later to the proper function, I guess.
     acc = history.history['accuracy']
     val_acc = history.history['val_accuracy']
@@ -155,7 +135,7 @@ def train_model(params,data,labels):
     return model
 
 
-def test(model,data):
+def test(model,data,params):
     '''
     Here we test, using model on the test data. Simply predict using the trained model.
 
@@ -164,42 +144,66 @@ def test(model,data):
     :param labels:
     :return: Returns a vector of predictions for the test data
     '''
-    probabilities = np.array(model.predict_proba(data,batch_size=32,verbose=1)) # computing the probabilities
+    probabilities = np.array(model.predict_proba(data,batch_size=params["test_batch_size"],verbose=1)) # computing the probabilities
     print(probabilities.shape)
     predictions = np.where(probabilities > 0.5, 1, 0) #computing the predictions from the model
     print(predictions.shape)
     return predictions,probabilities
 
-def errors(predictions,probabilities,test_images,test_labels):
+def precision_recall_graph(predictions, test_y):
     '''
-    finds the type1 and type 2 errors in the test data
+    This function  print precision vs recall graph
     :param predictions:
-    :param probabilities:
-    :param params:
-    :return: type_1: a vector of the 5 worse type 1 errors
-            type_2: a vector of the 5 worse type 2 errors
+    :param test_labels:
+    :return:
     '''
+    recall = dict()
+    precision = dict()
+
+    preds = np.array(predictions)
+    test_labels = np.array( test_y)
+
+    for i in range(2):
+        precision[i], recall[i], _ = precision_recall_curve(test_labels[i], preds[i])
+    precision["micro"], recall["micro"], _ = precision_recall_curve(test_labels.ravel(), preds.ravel())
+
+    plt.step(recall['micro'], precision['micro'], color='black', alpha=0.7, where='post')
+    plt.fill_between(recall["micro"], precision["micro"], alpha=0.3, color='orange',)
+    plt.xlabel('Recall')
+    plt.ylabel('Precision')
+    plt.title('Precision VS Recall Curve')
+    plt.show()
+
+def errors(predictions,probabilities,test_labels):
+    '''
+     finds the type1 and type2 errors in the test data
+    :param predictions: test predictions
+    :param probabilities: test probabilities
+    :param test_labels:the real labels
+    :return: top 5 errors from type1 and type2 errors and the images indices
+    '''
+
     type_1 = []
+    type_1_indices = []
     type_2 = []
-
-    for i in range(len(predictions)):
-        # miss detection - the alghorithem thought its not a flower  but it is
+    type_2_indices = []
+    for i in range(predictions):
+        # miss detection - the algorithem thought its not a flower  but it is
         if predictions[i] == 0 and test_labels[i] == 1:
-            type_1.append(probabilities[i],i)
-
-        # false alarm - the alghorithem thought its a flower but it is not
+            type_1.append(probabilities[i])
+            type_1_indices.append(i)
+        # false alarm - the algorithem thought its a flower but it is not
         if predictions[i] == 1 and test_labels[i] == 0:
-            type_2.append(probabilities[i],i)
-
+            type_2.append(probabilities[i])
+            type_2_indices.append(i)
     ## subset the 5 worse errors from type 1,2
     type_1_sorted = sorted(type_1, reverse=True, key=itemgetter(0))
     type_2_sorted = sorted(type_2, reverse=True, key=itemgetter(0))
     five_type_1 = type_1_sorted[0:5]
     five_type_2 = type_2_sorted[0:5]
-    print("type 1 errors")
-    print(five_type_1)
-    # need to save the images
-    return five_type_1, five_type_2
+
+    return five_type_1, five_type_2,type_1_indices,type_2_indices
+
 
 def evaluate(predicts, probabilities, real,params):
     '''
@@ -245,10 +249,6 @@ np.random.seed(42)
 params = GetDefaultParameters()
 train_images,train_labels,test_images,test_labels = load_data(params)
 model = train_model(params,train_images,train_labels)
-pred_prob, pred = test(model,test_images)
-'''
-precision,recall, thresholds = precision_recall_curve(test_labels.ravel(),pred_prob.ravel())
-print(precision)
-print(recall)
-print(thresholds)
-'''
+pred_prob, pred = test(model,test_images,params)
+precision_recall_graph(pred, test_labels)
+top5_type1,top5_type2, top5_type1_indices, top5_type2_indices = errors(pred,pred_prob,test_labels)